04152e8bb66e6e22e3f012dafe3792e083effec6
Alexander3006 <ukrpresident2036@gmail.com>
2021-01-13 22:13:53 +0200

    add: scripts tools for developing

---
+++ scripts/loadEnv.ps1
@@ -0,0 +1,16 @@
+
+$env:API_GATEWAY_PORT='8000'
+
+$env:USER_SERVICE_QUEUE='user_queue'
+$env:USER_SERVICE_HOST='user'
+
+$env:POSTGRES_DB_HOST = 'localhost'
+$env:POSTGRES_DB_PORT = '5432'
+$env:POSTGRES_DB_USER = 'postgres'
+$env:POSTGRES_DB_PASSWORD = '30062001'
+$env:POSTGRES_DB_NAME = 'Cablegram'
+
+$env:AMQP_HOST='localhost'
+$env:AMQP_PORT='5672'
+$env:AMQP_USER='guest'
+$env:AMQP_PASSWORD='guest'
---
+++ scripts/runRabbitmq.ps1
@@ -0,0 +1,1 @@
+docker run -d --hostname my-rabbit -p 15672:15672 -p 5672:5672 --name some-rabbit rabbitmq:3-management
