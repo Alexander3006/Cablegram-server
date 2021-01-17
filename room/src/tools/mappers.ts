b1d48160805e115ed279ad120c53e811b3380cdf
Alexander3006 <ukrpresident2036@gmail.com>
2021-01-17 19:09:38 +0200

    add: mappers domain model to dtos

---
+++ room/src/tools/mappers.ts
@@ -0,0 +1,30 @@
+import { RoomGrantDto } from "src/application/roomGrants/interfaces/roomGrants.dto";
+import { RoomDto } from "src/application/rooms/interfaces/rooms.dto";
+import { Room } from "src/domain/model/room/Room.aggregate";
+import { RoomGrant } from "src/domain/model/roomGrant/RoomGrant.aggregate";
+
+export const roomToRoomDto = (room: Room): RoomDto => {
+  const {id, roomTag, roomName} = room;
+  const roomDto = new RoomDto();
+  roomDto.id = id.id;
+  roomDto.name = roomName.name;
+  roomDto.tag = roomTag.tag;
+  return roomDto;
+}
+
+export const roomGrantToRoomGrantDto = (roomGrant: RoomGrant): RoomGrantDto => {
+    const {
+        id, 
+        roomGrantAuthor, 
+        roomGrantDelegat, 
+        roomGrantRole, 
+        roomId,
+    } = roomGrant;
+    const roomGrantDto = new RoomGrantDto();
+    roomGrantDto.id = id.id;
+    roomGrantDto.author = roomGrantAuthor.id;
+    roomGrantDto.delegat = roomGrantDelegat.id;
+    roomGrantDto.role = roomGrantRole.role;
+    roomGrantDto.room = roomId.id;
+    return roomGrantDto;
+}
