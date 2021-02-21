15769072ce484af3992fcf252a03187e29a6ed10
Alexander3006 <ukrpresident2036@gmail.com>
2021-02-21 18:54:46 +0200

    delete: test migrations

--- message/src/migrations/1612379216147-message.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class message1612379216147 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- message/src/migrations/1612379780244-message2.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class message21612379780244 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
